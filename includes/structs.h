02806da9358de7644b704b863b032dc66b8ac37a
Oleksii KUPIN <okupin@e1r11p1.unit.ua>
2019-10-03 22:05:13 +0300

    cleanup

--- includes/structs.h
+++
@@ -1,9 +0,0 @@
-
-// static struct sector
-// {
-//     float floorHeight;
-//     float ceilHeight;
-//     t_vt wall;
-//     signed char *neighbors;
-//     unsigned npoints;
-// } *sectors = NULL;
