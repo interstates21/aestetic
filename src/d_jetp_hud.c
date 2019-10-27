6c83bba922196365c5d128dd6cd853aaebb709be
Bogdan DEOMIN <bdeomin@e1r1p13.unit.ua>
2019-10-27 22:46:44 +0200

    ranaming file 3

--- src/d_jetp_hud.c
+++ src/d_jetp_hud.c
@@ -1,31 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   draw_hud.c                                         :+:      :+:    :+:   */
+/*   d_jetp_hud.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bdeomin <bdeomin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/22 23:10:02 by bdeomin           #+#    #+#             */
-/*   Updated: 2019/10/26 21:41:30 by bdeomin          ###   ########.fr       */
+/*   Updated: 2019/10/27 22:42:36 by bdeomin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/doom_nukem.h"
 
-static void	draw_ammo(t_data *d, t_weapon_type weapon_type)
-{
-	static char buf[100] = "Ammo: ";
-	static char buf2[100] = "Health: ";
-
-	ft_strcpy(buf + 6, ft_itoa_static(weapon_type.current_ammo));
-	ft_strcpy(buf + ft_strlen(buf), " / ");
-	ft_strcpy(buf + ft_strlen(buf), ft_itoa_static(MAX_BULLETS));
-	draw_string(d, (t_font){buf, 50, 80, 0, 2});
-	ft_strcpy(buf2 + 8, ft_itoa_static(d->player.health));
-	draw_string(d, (t_font){buf2, 50, 50, 0, 2});
-}
-
-static void	draw_fuel(t_data *d)
+void	draw_fuel(t_data *d)
 {
 	static char buf[100] = "Fuel: ";
 
@@ -33,7 +20,7 @@ static void	draw_fuel(t_data *d)
 	draw_string(d, (t_font){buf, 50, 110, 0, 2});
 }
 
-static void	draw_inventory_slot(t_data *d, SDL_Surface *tex, int x1, int y1)
+void	draw_inventory_slot(t_data *d, SDL_Surface *tex, int x1, int y1)
 {
 	uint32_t	px;
 	t_vec2		x_y;
@@ -53,24 +40,4 @@ static void	draw_inventory_slot(t_data *d, SDL_Surface *tex, int x1, int y1)
 				pixel_put(d, new_v3(x1 + x_y.x, y1 + x_y.y, 0), px, 0);
 		}
 	}
-}
-
-static void	draw_timed_msg(t_data *d)
-{
-	if (!d->msg[0])
-		return ;
-	draw_string(d, (t_font){d->msg, 50, HEIGHT - 180, 0, 2});
-	if (SDL_GetTicks() - d->msg_start > 3000)
-		d->msg[0] = 0;
-}
-
-void		draw_hud(t_data *d)
-{
-	if (d->slot3)
-		draw_inventory_slot(d, d->assets_texture[d->slot3->picnum],
-				140, HEIGHT - 100);
-	draw_timed_msg(d);
-	draw_ammo(d, d->weapon_type);
-	if (d->player.is_flying)
-		draw_fuel(d);
 }
