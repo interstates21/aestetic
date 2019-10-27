6ff875c63fa1d6ef882d967a2a97ee6f997af53d
Bogdan DEOMIN <bdeomin@e1r4p11.unit.ua>
2019-10-27 05:42:22 +0200

    global refact texanim.c

--- src/texanim.c
+++ src/texanim.c
@@ -1,20 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   texanim.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bdeomin <bdeomin@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/27 05:39:26 by bdeomin           #+#    #+#             */
+/*   Updated: 2019/10/27 05:40:33 by bdeomin          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/doom_nukem.h"
 
 void	precompute_texanim(t_data *d)
 {
 	int			i;
 	SDL_Surface	*s;
-	int			framenum;
-	int			numframes;
+	t_vec2		frames;
 
-	framenum = SDL_GetTicks() / 80;
+	frames.x = SDL_GetTicks() / 80;
 	i = -1;
 	while (++i < d->nb_textures)
 	{
 		s = d->textures[i];
-		if ((numframes = (s->h / s->w)) > 5)
-			s->userdata = (void*)(intptr_t)(framenum % numframes + 1);
-		else
+		frames.y = s->h / s->w;
+		if (frames.y <= 5)
 			s->userdata = 0;
+		else
+			s->userdata = (void*)(intptr_t)(frames.x % frames.y + 1);
 	}
 }
