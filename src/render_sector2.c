3f3d975b953b0098495ab7363d4a1f1fc8193eb7
Bogdan DEOMIN <bdeomin@e1r7p4.unit.ua>
2019-10-29 22:24:46 +0200

    .h refact 8

--- src/render_sector2.c
+++ src/render_sector2.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   loop.c                                             :+:      :+:    :+:   */
+/*   render_sector2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bdeomin <bdeomin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/28 20:11:45 by vslutiak          #+#    #+#             */
-/*   Updated: 2019/10/29 19:30:49 by bdeomin          ###   ########.fr       */
+/*   Updated: 2019/10/29 22:23:57 by bdeomin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/doom_nukem.h"
 
-t_sec* check_neighbor(t_env *d, int16_t nei)
+t_sec	*check_neighbor(t_env *d, int16_t nei)
 {
 	if (nei < 0 || nei >= d->n_sect)
 		return (NULL);
 	return (&d->sectors[nei]);
 }
 
-t_vec3f transform_back(t_env *d, t_vec3f v)
+t_vec3f	transform_back(t_env *d, t_vec3f v)
 {
 	return (new_v3(v.x * d->cam.cos + v.z * d->cam.sin + d->cam.pos.x, v.y,
 			v.x * -d->cam.sin + v.z * d->cam.cos + d->cam.pos.z));
